49a7c80a13c6f0cc2f8cacd37a43fcf3d166b118
Saiful Islam Auny <aunychowdhury99@gmail.com>
2025-04-22 19:50:41 +0600

    setting updated

--- smart_care/settings.py
+++ smart_care/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://smart-care-backend-hgly.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
